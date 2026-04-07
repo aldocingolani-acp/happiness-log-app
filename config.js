b75e960f3bd9bad60966f912e4f49b4c4e82f55f
aldocingolani-acp <aldo.cingolani@gmail.com>
2026-04-08 00:14:50 +0200

    Add runtime config file

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://lpisglhasfipmipavknh.supabase.co",
+  supabaseAnonKey: "sb_publishable_XDTSZtq8NLHTaqkQmqqvag_mCcwoc1e",
+  authRedirectTo: "",
+  webPushPublicKey: "",
+};
